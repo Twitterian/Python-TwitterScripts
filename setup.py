96b4850a10d6507467e4d2f76060dc5716606bb9
Usagination <tjddlf2645@gmail.com>
2016-04-06 14:31:16 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+print ('setup Twitterian...')
+setup (
+    name = 'Twitterian',
+    version = '1.0',
+    author='Twitterian',
+    author_email='usagination@gmail.com',
+    description = 'python scripts for advanced twitterian',
+    url='https://github.com/Twitterian/Python-TwitterScripts',
+    install_requires = [
+        'twython'
+        ],
+)
