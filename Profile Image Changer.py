fa2c588d683bd7b05fa9b2b997b410155e301229
Usagination <tjddlf2645@gmail.com>
2016-04-06 13:11:35 +0900

    Create Proffile Image Chagner.py with free icons

---
+++ Profile Image Changer.py
@@ -0,0 +1,36 @@
+﻿#!/usr/bin/python2
+# -*- coding:utf-8 -*-
+from twython import Twython
+from datetime import date, datetime, timedelta
+import time
+import os
+from os import listdir
+import random
+from os.path import isfile, join
+import configparser
+
+# read configs
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+twitter = Twython(
+    config['TwitterOauth']['AppToken'],
+    config['TwitterOauth']['AppSecret'],
+    config['TwitterOauth']['UserToken'],
+    config['TwitterOauth']['UserSecret']
+)
+delay = int(config['Nick Namer']['Delay'])
+
+# script body
+mypath = "Profile Image Change.Images"
+files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+while(len(files)) :
+    filename = files[random.randrange(0, len(files))]
+    try :
+        with open(mypath + '/' + filename, 'rb') as image:
+            twitter.update_profile_image(image=image)
+            print ('changed profile image to ' + filename)
+    except :
+        print ('error occured ' + filename)
+    time.sleep(delay)
