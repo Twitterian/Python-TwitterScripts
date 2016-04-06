e947cbe93c5b2b56fd9a67e5aa8287d49185f705
Usagination <tjddlf2645@gmail.com>
2016-04-06 13:10:51 +0900

    Creat AllowBlock.py

---
+++ AllowBlock.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+from twython import Twython
+from datetime import date, datetime, timedelta
+from Twitterian import Friendships;
+import configparser;
+
+# read configs
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+twitter = Twython(
+    config['TwitterOauth']['AppToken'],
+    config['TwitterOauth']['AppSecret'],
+    config['TwitterOauth']['UserToken'],
+    config['TwitterOauth']['UserSecret']
+)
+
+# script body
+friends = Friendships.GetFriendships(twitter);
+followers = Friendships.GetFollowers(twitter);
+
+print('')
+print('following : %d, follower : %d' % (len(friends), len(followers)))
+
+unfollowedUserCount = 0
+bubedUserCount = 0
+blockedUserCount = 0
+followedUserCount = 0
+
+for user in followers:
+    if user in friends:
+        continue
+    else:
+        suser = twitter.show_user(user_id=user)
+        print('----------------------------------------')
+        print("Name : " + suser["name"])
+        print('Username : @' + suser["screen_name"])
+        print('Description : ' + suser["description"])
+        print('Friends : ' + str(suser["friends_count"]) +
+              "\tFollowers : " + str(suser["followers_count"]))
+        print('Created at : ' + suser["created_at"])
+        print('Protected : ' + str(suser["protected"]))
+        print('')
+        print('  [0] Do nothing')
+        print('  [1] BUB this user')
+        print('  [2] Block this user')
+        print('  [3] Follow this user')
+        print('')
+
+        while True:
+            try:
+                sel = int(input("select csae : "))
+
+                if sel == 0:
+                    print("pass user @%s(%d)" % (suser["screen_name"], user))
+                    break
+                elif sel == 1:
+                    twitter.create_block(screen_name=user['screen_name'])
+                    twitter.destroy_block(screen_name=user['screen_name'])
+                    print("block and unblock @%s(%d)" % (suser["screen_name"], user))
+                    break
+                elif sel == 2:
+                    twitter.create_block(screen_name=user['screen_name'])
+                    print("block user @%s(%d)" % (suser["screen_name"], user))
+                    break
+                elif sel == 3:
+                    twitter.create_friendship(screen_name=user['screen_name'])
+                    print("follow user @%s(%d)" % (suser["screen_name"], user))
+                    message = input("\ndo yo wanna mention to this user? if you dont wanna, you can input empty space : ")
+                    if message != '':
+                        message = "@" + suser["screen_name"] + " " + message
+                        twitter.update_status(status=message)
+                        print("tweet [" + message + "]")
+                        break
+                    break
+                else:
+                    print("invaild input. retry.")
+                    continue
+            except ValueError:
+                print("invaild input. retry.")
+
+
+print('')
+print(str(bubedUserCount) + "user(s) bubed.")
+print(str(blockedUserCount) + "user(s) blocked.")
+print(str(followedUserCount) + "user(s) followed.")
+print('')
+input("press any key to continue")
